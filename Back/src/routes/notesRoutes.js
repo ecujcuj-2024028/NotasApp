431a88389b8318c17ee24d5e248366d4c7e0af89
ecujcuj-2024028 <ecujcuj-2024028@kinal.edu.gt>
2026-07-01 20:39:11 -0600

    feat: CRUD de notas y configuracion para mongo

---
+++ Back/src/routes/notesRoutes.js
@@ -0,0 +1,80 @@
+import express from "express";
+import Note from "../models/noteModels.js";
+const router = express.Router();
+
+//Obtener todas las notas
+router.get("/", async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error al obtener las notas", error);
+        res.status(500).json({ message: "Error al obtener las notas" })
+    }
+});
+
+
+//Obtener una nota por Id
+router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const note = await Note.findById(id);
+        if (!note) {
+            return res.status(404).json({ message: "Nota no encontrada" });
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        console.error("Error al obtener la nota", error);
+        res.status(500).json({ message: "Error al obtener la nota" });
+    }
+});
+
+
+//Crear una nota
+router.post("/", async (req, res) => {
+    try {
+        const { title, description } = req.body;
+        const note = new Note({ title, description });
+
+        const savedNote = await note.save()
+
+        if (savedNote) {
+            res.status(201).json({ message: "Nota creada correctamente", note: savedNote });
+        }
+    } catch (e) {
+        console.error("Error al crear la nota", e);
+        res.status(500).json({ message: "Error al crear la nota" });
+    }
+})
+
+// Eliminar una nota
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedNote = await Note.findByIdAndDelete(id);
+        if (!deletedNote) {
+            return res.status(404).json({ message: "Nota no encontrada" });
+        }
+        res.status(200).json({ message: "Nota eliminada correctamente" });
+    } catch (error) {
+        console.error("Error al eliminar la nota", error);
+        res.status(500).json({ message: "Error al eliminar la nota" });
+    }
+})
+
+// Editar una nota
+router.put("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { title, description } = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(id, { title, description }, { new: true });
+        if (!updatedNote) {
+            return res.status(404).json({ message: "Nota no encontrada" });
+        }
+        res.status(200).json({ message: "Nota actualizada correctamente", note: updatedNote });
+    } catch (error) {
+        console.error("Error al editar la nota", error);
+        res.status(500).json({ message: "Error al editar la nota" });
+    }
+})
+export default router;
--- Back/src/server.js
+++ Back/src/server.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server en el puerto ${PORT}`);
-})
+app.use(express.json()); // Para poder leer el cuerpo de las solicitudes
+app.use("/api/notes", notesRoutes);
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server en el puerto ${PORT}`);
+        })
+    })
+
+
+
